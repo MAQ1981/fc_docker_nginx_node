8a2f3e144b24d038d7a02bea6c2534fd75af2476
MAQ1981 <141975453+MAQ1981@users.noreply.github.com>
2023-08-31 01:04:18 +0000

    Versão final desafio docker node nginx

--- node/index.js
+++ node/index.js
@@ -10,21 +10,26 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
+
+
 const sql = `insert into people(name) values ?`
 const peoples = [['Marcelo'], ['Quinalha'], ['MAQ']]
 connection.query(sql, [peoples])
 
-const sqlSelect = `SELECT * FROM people ORDER BY 1 ASC`
-connection.query(sqlSelect)
-
-connection.end()
-
+//const sqlSelect = `SELECT * FROM people ORDER BY 1 ASC`
+//connection.query(sqlSelect)
 
 app.get('/', (req,res) => {
     res.send('<h1>Full Cycle Rocks</h1>')
 })
 
+connection.end()
+
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
 })
+
+
+
+
 
